a75987498c5f95ce7ab950ead88023dd088000f0
chiefkarim <krimotop2016@gmail.com>
2023-04-17 09:09:43 +0100

    integrated with firebase

--- where-s-waldo/src/App.js
+++ where-s-waldo/src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD7F60Y15XOih_foe7cmY3k9LFsb-tybDw",
+    authDomain: "where-s-waldo-38875.firebaseapp.com",
+    projectId: "where-s-waldo-38875",
+    storageBucket: "where-s-waldo-38875.appspot.com",
+    messagingSenderId: "953225963954",
+    appId: "1:953225963954:web:1bbd35f3b307db3d521cab",
+    measurementId: "G-H1FCPCGW35",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log(analytics);
   return (
     <div className="App">
       <header className="App-header">
